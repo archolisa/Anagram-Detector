3d980cb0878f639e3f199f499b88aa07d68e8f07
archolisa <105179700+archolisa@users.noreply.github.com>
2022-05-28 16:00:11 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# Check if two words are anagrams
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+word = input("Enter first string: ")
+anagram = input("Enter second string: ")
+
+def find_anagram(word,angram):
+    if (sorted(word) == sorted(anagram)):
+     return True
+    else:
+        return False
+print(find_anagram(word,anagram))
